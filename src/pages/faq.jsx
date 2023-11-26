98ad4bdf552253f6153d8e9cde2f953caec24af1
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-11-26 09:13:44 -0300

    feat: add index and faq page

---
+++ src/pages/faq.jsx
@@ -0,0 +1,11 @@
+import Link from "../src/components/Link"
+export default function Faq() {
+    return (
+        <div>
+            <h1>Faq</h1>
+            <Link href="/">
+                Ir para home
+            </Link>
+        </div>
+    )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,30 @@
+import Link from "../src/components/Link";
+
+function Title({children, as}){
+
+    const Tag = as;
+    return (
+        <>        
+        <Tag>
+            {children}
+        </Tag>
+        <style jsx>{`
+            ${Tag} {
+                color: red;
+            }
+        `}
+        </style>        
+        </>
+    )
+}
+
+export default function Homepage() {
+    return (
+        <div>
+        <Title as="h3">Alura Cases - Home</Title>
+        <Link href="/faq">
+            Ir para pagina FAQ
+        </Link>
+        </div>
+    )
+}
