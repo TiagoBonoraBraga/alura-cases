384f8e4b801355ff85f09c5ff273c51e882f0d35
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-11-26 09:15:16 -0300

    feat: create 404 page and Link component

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import Link from "../src/components/Link"
+export default function Faq() {
+    return (
+        <div>
+            <h1>Você c perdeu.... Volte para Home no link abaixo ERROR 404</h1>
+            <Link href="/">
+                Ir para home
+            </Link>
+        </div>
+    )
+}
